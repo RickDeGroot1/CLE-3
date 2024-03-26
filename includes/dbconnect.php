f641625d630aed1d7f8bd3f99efa327693095ccf
RickDeGroot9003557 <90246463+RickDeGroot9003557@users.noreply.github.com>
2024-03-26 11:07:53 +0100

    added dbconnect include

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,1 +1,8 @@
 <?php
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "CLE_3";
+
+$db = mysqli_connect($host, $user, $password, $database)
+or die("Error: " . mysqli_connect_error());
